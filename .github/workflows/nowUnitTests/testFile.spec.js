351b0da712477af63d63c4c73a0019273dd3d74c
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-07-10 16:48:35 +0000

    testing file

---
+++ .github/workflows/nowUnitTests/testFile.spec.js
@@ -0,0 +1,6 @@
+describe('Now Unit Hello Test', () => {
+  it('should print Hello to Now Unit', () => {
+    console.log('Hello to Now Unit');
+    expect(true).toBe(true); // dummy assertion
+  });
+});
