56a3ff467db7530fc2f68e4166461f3bfcfc1c50
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-07-13 23:25:49 +0530

    Update mathUtil1.spec.js

--- __tests__/mathUtil1.spec.js
+++ __tests__/mathUtil1.spec.js
@@ -1,1 +1,10 @@
+const funcs = require('../src/mathUtil1'); // Adjust path if needed
+
+test('Add test', () => {
+  funcs.add(2, 3); // no expect
+});
+
+test('Sub test', () => {
+  funcs.subtract(9, 4); // no assertion
+});
 
