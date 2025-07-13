b46ebe27e5099c6b184b1a4e134a4452976025d5
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-07-13 16:46:20 +0530

    Update mathUtil2.spec.js

--- __tests__/mathUtil2.spec.js
+++ __tests__/mathUtil2.spec.js
@@ -1,7 +1,7 @@
 const mathUtil = require('../src/mathUtil2');
 
 describe('mathUtil', () => {
-  it('should add two numbers', () => {
+  it('should  add two numbers', () => {
     mathUtil.add(2, 3);
   });
 });
