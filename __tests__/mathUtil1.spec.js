26146677cb75bad32408c086fcbe4fff3d7166c0
sairamcse001 <sairam.pasupuleti@servicenow.com>
2025-07-13 23:22:44 +0530

    Update mathUtil1.spec.js

--- __tests__/mathUtil1.spec.js
+++ __tests__/mathUtil1.spec.js
@@ -1,9 +1,1 @@
-const mathUtil = require('../src/mathUtil1');
 
-describe('mathUtil', () => {
-  it('should add two numbers', () => {
-    expect(mathUtil.add(2, 3)).toBe(7);
-  });
-
-  // multiply is not tested → intentional partial coverage
-});
